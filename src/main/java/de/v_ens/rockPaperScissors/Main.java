0926a13780c17f0df23188beb3f0c266c6d79262
Viktor Ens <v.ens86@yahoo.de>
2024-04-24 16:13:34 +0200

    add rock paper scissors

---
+++ src/main/java/de/v_ens/rockPaperScissors/Main.java
@@ -0,0 +1,23 @@
+package de.v_ens.rockPaperScissors;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(rps("rock", "scissors"));
+    }
+
+    public static String rps(String p1, String p2) {
+        if (p1.equals(p2)) {
+            return "Draw!";
+        }
+        if (p1.equals("rock") && p2.equals("scissors")) {
+            return "Player 1 won!";
+        }
+        if (p1.equals("scissors") && p2.equals("paper")) {
+            return "Player 1 won!";
+        }
+        if (p1.equals("paper") && p2.equals("rock")) {
+            return "Player 1 won!";
+        }
+        return "Player 2 won!";
+    }
+}
