f27ffdbe734ff1768e1fc82588525d24e128758e
Viktor Ens <v.ens86@yahoo.de>
2024-04-26 14:02:38 +0200

    add list exercise

---
+++ src/main/java/de/v_ens/lists/Main.java
@@ -0,0 +1,18 @@
+package de.v_ens.lists;
+
+/*
+Step 1: Create a Java class "Student" that should have the following properties: First name, Last name, Student ID.
+Step 2: Create a Java List for elements of type "Student" and add multiple students.
+Step 3: Create a class "School" to which students can be added through a method ('addStudent').
+Step 4: Implement a method to print all students of the school (as text, to the console).
+Step 5: Add a method to find a student by their ID. The found student should be returned.
+Step 6: Create a method to remove a student from the school.
+### Bonus ###
+-Expand the Java-List project with a class called "Course" that has the following properties: course name, instructor, room.
+-Add a method to the Student class to add courses to the student (courses in which the student is participating).
+-Implement a method within the School class to retrieve all courses of a specific student (based on a student ID).
+ */
+
+
+public class Main {
+}
