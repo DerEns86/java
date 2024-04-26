d8b8e17d8249fbd8e1703cc93812045d464b8f61
Viktor Ens <v.ens86@yahoo.de>
2024-04-26 14:02:10 +0200

    add GameBoard

---
+++ src/main/java/de/v_ens/interfaces/gameExercise/GameBoard.java
@@ -0,0 +1,25 @@
+package de.v_ens.interfaces.gameExercise;
+
+public class GameBoard {
+
+    private static final int SIZE = 10; // Define the size of the gameboard
+    private char[][] board;
+
+    public GameBoard() {
+        board = new char[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                board[i][j] = '-'; // Initialize all positions as empty
+            }
+        }
+    }
+
+    public void printBoard() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
--- src/main/java/de/v_ens/interfaces/gameExercise/Main.java
+++ src/main/java/de/v_ens/interfaces/gameExercise/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 
+        GameBoard gameBoard = new GameBoard();
+        gameBoard.printBoard();
+
         Character c1 = new Character(0, 0, "Character1");
 
         Player p1 = new Player(0, 0, "Player1", "Sword");
@@ -27,3 +30,17 @@ public class Main {
 
     }
 }
+
+//TODO
+// refactore the movable
+// refactore the character class to implement health
+// add a new interface fightable with methods to fight and change weapon
+// add a new class Enemy that implements fightable and movable
+// check if player and enemy are in the same position and let them fight
+// if the player wins, the enemy should be removed from the game
+// if player and npc are in the same position, the npc should introduce itself
+// add a new class Game that manages the game
+// add a new class gameboard that represents the gameboard
+// visualize the gameboard in the console
+
+
