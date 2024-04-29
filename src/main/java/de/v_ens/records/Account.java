95cbd466072f2e76be410e56df126dc793760e80
Viktor Ens <v.ens86@yahoo.de>
2024-04-29 11:44:12 +0200

    finish record bank Account exercise

---
+++ src/main/java/de/v_ens/records/Account.java
@@ -0,0 +1,62 @@
+package de.v_ens.records;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Account {
+
+    private String accountNumber;
+    private BigDecimal balance;
+    private Client customer;
+
+    public Account(String accountNumber, double balance, Client customer) {
+        this.accountNumber = accountNumber;
+        this.balance = BigDecimal.valueOf(balance);
+        this.customer = customer;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public Client getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Client customer) {
+        this.customer = customer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Account account)) return false;
+        return Objects.equals(accountNumber, account.accountNumber) && Objects.equals(balance, account.balance) && Objects.equals(customer, account.customer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accountNumber, balance, customer);
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "accountNumber='" + accountNumber + '\'' +
+                ", balance=" + balance +
+                ", customer=" + customer +
+                '}';
+    }
+}
--- src/main/java/de/v_ens/records/Main.java
+++ src/main/java/de/v_ens/records/Main.java
@@ -5,5 +5,15 @@ package de.v_ens.records;
 Create a class 'Account' (not a record, should initially be mutable) with the properties Account Number (String), Account Balance (BigDecimal), and the associated customer.*/
 
 
+import java.math.BigDecimal;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Client client1 = new Client("John", "Doe", "1234");
+        Account account1 = new Account("123456", 1000.03, client1);
+        System.out.println(account1);
+
+        account1.setBalance(account1.getBalance().add(BigDecimal.valueOf(100)));
+    }
 }
