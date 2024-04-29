cf749c70d81ce9217b04d573b449d6eb190ef7af
Viktor Ens <v.ens86@yahoo.de>
2024-04-29 11:36:34 +0200

    add record exercise

---
+++ src/main/java/de/v_ens/records/Client.java
@@ -0,0 +1,5 @@
+package de.v_ens.records;
+
+public record Client(String firstName, String lastName, String id) {
+
+}
---
+++ src/main/java/de/v_ens/records/Main.java
@@ -0,0 +1,9 @@
+package de.v_ens.records;
+
+/*Create a record 'Client' with the properties First Name, Last Name, and Customer Number (choose suitable English field names).
+
+Create a class 'Account' (not a record, should initially be mutable) with the properties Account Number (String), Account Balance (BigDecimal), and the associated customer.*/
+
+
+public class Main {
+}
